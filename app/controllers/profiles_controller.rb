d7af71435d5940effcbb69820784c05e9c6da002
evetodew <evetodew@gmail.com>
2015-09-07 14:13:26 +0000

    Add paperclip gem for image uploads

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_current_user
     def new
         # form where a user can fill out their own profile
         @user = User.find(params[:user_id])
@@ -16,6 +18,16 @@ class ProfilesController < ApplicationController
         end
     end
     
+    def update
+        @user = User.find(params[:user_id])
+        @profile = @user.profile
+        if @profile.update_attributes(profile_params)
+            flash[:success] = "Profile updated!"
+            redirect_to user_path(params[:user_id])
+        else
+            render action: :edit
+        end
+    end
     
     def edit
         @user = User.find (params[:user_id])
@@ -25,5 +37,10 @@ class ProfilesController < ApplicationController
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
+        
+        def only_current_user
+           @user = User.find(params[:user_id])
+           redirect_to(root_url) unless @user == current_user
         end
 end
